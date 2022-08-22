c0cf0e2a24dfd9d1c87494e102dc2bdde8c0067c
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-22 21:58:03 +0300

    add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45-->101101
+//3-->11
+//2-->10
+
+void MakeBinary()
+{
+    Console.Write("Введите натуральное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    string binary = string.Empty;
+    int n = number;
+    while(n > 0)
+    {
+        binary = $"{n % 2}" + binary;
+        n /= 2;
+    }
+    Console.WriteLine ($"{number} --> {binary}");
+}
+MakeBinary();
